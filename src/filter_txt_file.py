faed3cb90431114cddae4e6cda697084a2a8bd5e
ViktorShadr <v.viktor.shadrin@gmail.com>
2025-06-27 15:04:19 +0300

    fixed filter_txt_file.py by linters

--- src/filter_txt_file.py
+++ src/filter_txt_file.py
@@ -4,18 +4,10 @@ import re
 def filter_txt_file(txt_file):
     list_txt = re.split(r'[\n#\s]', txt_file)
     list_clean_symbols = list(filter(lambda x: x.lstrip('-').isdigit(), list_txt))
-    list_negative_number_by_zero = [int(x) if int(x) >= 0 else 0 for x in list_clean_symbols]
 
-
-
-    # print(list_txt)
-    # print(list_clean_symbols)
-    # print(type(list_clean_symbols))
-    # print(list_negative_number_by_zero)
-    # # print(sum_list)
-
-    return
+    return [int(x) if int(x) >= 0 else 0 for x in list_clean_symbols]
 
 
 with open('../data/nums.txt', 'r', encoding='utf-8') as file:
-    filter_txt_file(file.read())
+    sum_list = float(sum(filter_txt_file(file.read())))
+    print(f'the sum is {sum_list}')
