67e63d33b4f3903dff0ed102d67390946cfa90a9
Viktor Shadrin <v.vshadrin@gmail.com>
2025-06-27 21:06:55 +0300

    fixed filter_txt_file.py as lesson

--- src/filter_txt_file.py
+++ src/filter_txt_file.py
@@ -5,7 +5,7 @@ def filter_txt_file(txt_file):
     list_txt = re.split(r'[\n#\s]', txt_file)
     list_clean_symbols = list(filter(lambda x: x.lstrip('-').isdigit(), list_txt))
 
-    return [int(x) if int(x) >= 0 else 0 for x in list_clean_symbols]
+    return (int(x) if int(x) >= 0 else 0 for x in list_clean_symbols)
 
 
 with open('../data/nums.txt', 'r', encoding='utf-8') as file:
